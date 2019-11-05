3965b3188cec7a448524d464ed77c26157bdcda4
Roope <roope-kettu@hotmail.com>
2019-11-05 11:38:31 +0200

    Added velocity controls and falling if velocity too low. Needs fine tuning.

--- Demos/FlightControlsDemo/PlayerControls.cs
+++ Demos/FlightControlsDemo/PlayerControls.cs
@@ -1,20 +1,25 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    private float boostInput;
+    bool speedUp;
+    bool speedDown;
     private float pitchInput;
     private float yawInput;
     private float rollInput;
-    private float speed = 0.1f;
+    private float speed = 0.5f;
     private float pitchSpeed = 1f;
     private float rollSpeed = 1.5f;
     private float yawSpeed = 1f;
+    private float fallingSpeed = 0f;
+    /*
     private float x = 0f;
     private float y = 0f;
     private float z = 0f;
+    */
 
     // Start is called before the first frame update
     void Start()
@@ -24,18 +29,28 @@ public class PlayerControls : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (speed < 0.3f)      // If speed gets too low, plane starts droppin
+        {
+            fallingSpeed = -0.004f / Math.Max(speed, 0.01f);
+        }
+        else
+        {
+            fallingSpeed = 0f;
+        }
+        if (speedUp) speed += 0.01f;
+        if (speedDown) speed -= 0.01f;
+
         transform.Translate(0f, 0f, speed);
-        //transform.Rotate(pitchSpeed*pitchInput, yawSpeed*yawInput, -rollSpeed*rollInput, Space.Self);
-        transform.rotation = Quaternion.Euler(x, y, z);
+        transform.Rotate(pitchSpeed * pitchInput, yawSpeed * yawInput, -rollSpeed * rollInput, Space.Self);
+        transform.position += new Vector3(0f, fallingSpeed, 0f);
+        //transform.rotation = Quaternion.Euler(x, y, z);   // x, y from accelerometer
     }
 
     // Update is called once per frame
     void Update()
     {
-        x += 0.1f;
-        y += 0.01f;
-        z += 0.01f;
-        boostInput = Input.GetAxis("Jump");
+        speedUp = Input.GetKeyDown(KeyCode.X);
+        speedDown = Input.GetKeyDown(KeyCode.Z);
         pitchInput = Input.GetAxis("Pitch");
         rollInput = Input.GetAxis("Roll");
         yawInput = Input.GetAxis("Yaw");
