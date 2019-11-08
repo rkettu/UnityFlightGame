d68dcec20191b15b4e8c8843cec2ff4eac77be6d
Roope <roope-kettu@hotmail.com>
2019-11-08 13:21:30 +0200

    Reorganized folders, added script for Coin movement

---
+++ Unity Scripts/CoinMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinMovement : MonoBehaviour
+{
+    private float max;
+    private float min;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float movement_height = 0.75f;
+        max = transform.position.y + movement_height;
+        min = max - movement_height * 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time * 0.75f, max - min) + min, transform.position.z);
+        transform.Rotate(new Vector3(0, 80, 0) * Time.deltaTime);
+    }
+
+}
