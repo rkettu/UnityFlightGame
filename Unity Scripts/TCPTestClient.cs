cf2175ed9eabc66ab3bf6a86ad2042d666f236c7
jepperi <jesper.ruuska@gmail.com>
2019-11-12 12:41:36 +0200

    added TCP connection script

---
+++ Unity Scripts/TCPTestClient.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class TCPTestClient : MonoBehaviour
+{
+    [SerializeField] private string TCP_host = "192.168.1.1";
+    [SerializeField] private int TCP_port = 5000;
+    [SerializeField] private int buffer_size = 1024;
+    #region private members 	
+    private TcpClient socketConnection;
+    private Thread clientReceiveThread;
+    #endregion
+
+    private string serverMessage;
+
+    public string rasp_data
+    {
+        get
+        {
+            return rasp_data;
+        }
+    }
+
+    // Use this for initialization 	
+    void Start()
+    {
+        ConnectToTcpServer();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnDestroy()
+    {
+        socketConnection.Close();
+        clientReceiveThread.Abort();
+    }
+    /// <summary> 	
+    /// Setup socket connection. 	
+    /// </summary> 	
+    private void ConnectToTcpServer()
+    {
+        try
+        {
+            clientReceiveThread = new Thread(new ThreadStart(ListenForData));
+            clientReceiveThread.IsBackground = true;
+            clientReceiveThread.Start();
+        }
+        catch (Exception e)
+        {
+            Debug.Log("On client connect exception " + e);
+        }
+    }
+    /// <summary> 	
+    /// Runs in background clientReceiveThread; Listens for incomming data. 	
+    /// </summary>     
+    private void ListenForData()
+    {
+        try
+        {
+            socketConnection = new TcpClient(TCP_host, TCP_port);
+            Byte[] bytes = new Byte[buffer_size];
+            while (true)
+            {
+                // Get a stream object for reading 				
+                using (NetworkStream stream = socketConnection.GetStream())
+                {
+                    int length;
+                    // Read incomming stream into byte arrary. 					
+                    while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        var incommingData = new byte[length];
+                        Array.Copy(bytes, 0, incommingData, 0, length);
+                        // Convert byte array to string message. 
+                        serverMessage = Encoding.UTF8.GetString(incommingData);
+                        Debug.Log("server message received as: " + serverMessage);
+                    }
+                }
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("Socket exception: " + socketException);
+        }
+    }
+    
+}
