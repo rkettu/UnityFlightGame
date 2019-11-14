c376c15f65e3f27fcdb434e0e5d7fe19f89a8ed2
Roope <roope-kettu@hotmail.com>
2019-11-14 13:24:44 +0200

    Added script for playing a sound effect

---
+++ Unity Scripts/PlaySoundEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 
+ *  CAN BE USED TO PLAY SOUNDS, CALL FUNCTIONS FROM ANOTHER SCRIPT
+ *  USEFUL FOR PLAYING SOUNDS EMITTED FROM DESTROYING OBJECTS
+ *  ATTACH TO AN EMPTY GAMEOBJECT
+ *  ADD SOUND FILE TO AudioClip IN INSPECTOR
+ *  ADD AudioSource COMPONENT
+ */
+public class PlaySoundEffect : MonoBehaviour
+{
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip coinSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void playCoinSound()
+    {
+        audioSource.PlayOneShot(coinSound, 1f);
+    }
+}
