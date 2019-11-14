f2c383079123b1abf90076c9f179a0cf762ca4ce
Roope <roope-kettu@hotmail.com>
2019-11-14 11:12:47 +0200

    Added script for pausing game with esc. Upon pressing esc all child elements are shown and time is stopped. On second press, child elements are hidden and time resumes.

---
+++ Unity Scripts/PauseScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * 
+ *  HIDES/SHOWS ALL CHILD OBJECTS AND PAUSES/UNPAUSES ON ESC
+ *  APPLY TO PARENT OBJECT WITH PAUSE CANVAS AS ITS CHILDREN
+ * 
+ */
+public class PauseScript : MonoBehaviour
+{
+    private bool gamePaused = false;
+    void Start()
+    {
+        closePause();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!gamePaused)
+            {
+                openPause();
+                gamePaused = true;
+            }
+            else if (gamePaused)
+            {
+                closePause();
+                gamePaused = false;
+            }
+        }
+    }
+
+    private void closePause()
+    {
+        Time.timeScale = 1;
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    private void openPause()
+    {
+        Time.timeScale = 0;
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+    }
+}
