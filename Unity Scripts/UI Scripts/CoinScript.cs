b18ba3eb48301b4e0ec2662bdd61c24a2554d436
Tapani <t8sima01@students.oamk.fi>
2019-11-14 11:21:09 +0200

    Unity Scriptejä lisätty

---
+++ Unity Scripts/UI Scripts/CoinScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    private static int points = 0;
+    static Text scoreValue;
+    public float timer = 0f;
+    Text timerValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerValue = transform.FindChild("TimeText").GetComponent<Text>();
+        scoreValue = transform.FindChild("ScoreText").GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //timer = timer + 1 / 30;
+        timer += Time.deltaTime;
+        string time = "Time: "+timer.ToString("F2");
+        timerValue.text = time;
+        Debug.Log(time);
+       
+    }
+
+    public static void updateCoins()
+    {
+        points++;
+        scoreValue.text = "Score: " + points;
+    }
+
+}
