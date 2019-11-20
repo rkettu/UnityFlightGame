e9f7090ff408906319a44ad60406fefb03e02b4b
Roope <roope-kettu@hotmail.com>
2019-11-20 13:33:06 +0200

    Improved pitch/roll script

--- Nucleo Scripts/PitchRollDemo/main.cpp
+++ Nucleo Scripts/PitchRollDemo/main.cpp
@@ -1,9 +1,12 @@
 #include "mbed.h"
 #include "L3G4200D.h"
+#include "HMC5883L.h"
 #define M_PI 3.14159
 
 Serial pc(USBTX,USBRX);
 L3G4200D gyro(D14, D15);
+HMC5883L magneto(D2, D8);
+
 Timer timer;
 
 DigitalOut Vcc(A0);
@@ -13,7 +16,7 @@ AnalogIn zIn(A3);
 DigitalOut Gnd(A4);
 
 int g[3] = {0,0,0};
-
+float filterWeight = 0.1;
 
 int main()
 {
@@ -23,9 +26,7 @@ int main()
 	
 	float roll = 0;
 	float pitch = 0;
-	float gyro_roll = 0;
-	float gyro_pitch = 0;
-	
+
 	while(true)
     {
 		float aika = timer.read_ms();        
@@ -33,25 +34,25 @@ int main()
 		
 		gyro.read(g);
 		
-		g[0] = g[0] - 4;
-		g[1] = g[1] + 6;
-		g[2] = g[2] - 6;
+		// Vakiotason korjaukset
+		//g[0] = g[0] + 0;
+		g[1] = g[1] + 11;
+		//g[2] = g[2] + 2;
 
 		float ax = 98.585 * xIn.read() - 48.307;
 		float ay = 95.243 * yIn.read() - 48.288;
 		float az = 101.1 * zIn.read() - 51.056;
 				
-		gyro_pitch += g[1] * (aika / 1000) * 0.070054;
-		gyro_roll += g[0] * (aika / 1000) * 0.070054;
+		pitch += g[1] * (aika / 1000) * 0.071174;
+		roll += g[0] * (aika / 1000) * 0.071174;
 				
-		float acc_pitch = 180 * atan (ax/sqrt(ay*ay + az*az))/M_PI;
-		float acc_roll = 180 * atan (ay/sqrt(ax*ax + az*az))/M_PI;
-		
+		float accPitch = 180 * atan (ax/sqrt(ay*ay + az*az))/M_PI;
+		float accRoll = 180 * atan (ay/sqrt(ax*ax + az*az))/M_PI;
 				
-		pitch = 0.8 * (pitch + gyro_pitch * aika/1000) + 0.2 * acc_pitch;
-	    roll = 0.8 * (roll + gyro_roll * aika/1000) + 0.2 * acc_roll;
-				
-		pc.printf("%f %f \n", pitch, roll);
+		pitch = (1-filterWeight) * pitch + filterWeight * accPitch;
+	    roll = (1-filterWeight) * roll + filterWeight * accRoll;
+			
+		pc.printf("%.2fs%.2f\n", pitch, roll);
 
 	}
 }
