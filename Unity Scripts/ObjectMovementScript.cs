6b23fff9a384cd71497896e3e67244a09bbd7f04
Roope <roope-kettu@hotmail.com>
2019-12-03 13:59:40 +0200

    Added script for back-and-forth object movement

---
+++ Unity Scripts/ObjectMovementScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovementScript : MonoBehaviour
+{
+    
+    private float movementX = 30f;  
+    private float movementFreq = 0.5f;    // Try values under 1f
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }      
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * Mathf.Sin(movementFreq * Time.time) * movementX);
+        //transform.position = new Vector3(originalPos + Mathf.Lerp(0, movementX, (Mathf.Sin(movementSpeed * Time.time) + 1.0f) / 2.0f), transform.position.y, transform.position.z);
+    }
+}
